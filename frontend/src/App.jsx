6736c8593aaf9913f747785b7bc0e9cd4e909f80
ankitatech-21 <ankitasainik20@gmail.com>
2026-09-25 23:11:50 +0530

    Add diabetic retinopathy screening frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,69 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/public/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+import OperatorDashboard from "./pages/operator/OperatorDashboard";
+import NewScreening from "./pages/operator/NewScreening";
+import Screening from "./pages/operator/Screening";
+import ScreeningResult from "./pages/operator/ScreeningResult";
+import Explainability from "./pages/operator/Explainability";
+
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import ReviewQueuePage from "./pages/doctor/ReviewQueuePage";
+import PatientReviewPage from "./pages/doctor/PatientReviewPage";
+
+function App() {
+  return (
+    <Routes>
+      {/* Public */}
+      <Route path="/" element={<Home />} />
+
+      {/* Authentication */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      {/* Screening Operator */}
+      <Route path="/operator" element={<OperatorDashboard />} />
+
+      <Route
+        path="/operator/new-screening"
+        element={<NewScreening />}
+      />
+
+      <Route
+        path="/operator/screening"
+        element={<Screening />}
+      />
+
+      <Route
+        path="/operator/result"
+        element={<ScreeningResult />}
+      />
+
+      <Route
+        path="/operator/explainability"
+        element={<Explainability />}
+      />
+
+      {/* Ophthalmologist */}
+      <Route
+        path="/doctor"
+        element={<DoctorDashboard />}
+      />
+
+      <Route
+        path="/doctor/reviews"
+        element={<ReviewQueuePage />}
+      />
+
+      <Route
+        path="/doctor/review/:patientId"
+        element={<PatientReviewPage />}
+      />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/doctor/PatientReviewPage.jsx
@@ -0,0 +1,3 @@
+export default function PatientReviewPage() {
+  return <main><h1>Patient Review</h1><p>AI result, Grad-CAM, lesion evidence and doctor assessment.</p></main>;
+}
